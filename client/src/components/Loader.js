d182b4a16c1c61d6a661e27f69c0fba940978337
nsalasz <leonelson951@gmail.com>
2024-10-28 17:57:05 -0500

    add

---
+++ client/src/components/Loader.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div className='h-screen flex items-center justify-center fixed inset-0 bg-primary z-[10000]'>
+        <div className='flex gap-5 text-6xl font-semibold sm:text-3xl'>
+        <h1 className='text-secondary text-4xl font-semibold'>N</h1>
+        <h1 className='text-white text-4xl font-semibold'>N</h1>
+        <h1 className='text-tertiary text-4xl font-semibold'>S</h1>
+        <h1 className='text-tertiary text-4xl font-semibold'>Z</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
---
+++ client/src/pages/Home/LeftSider.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function LeftSider() {
+  return (
+    <div className="fixed left-0 bottom-0 px-10 sm:static">
+      <div className="flex flex-col items-center">
+        <div className="flex flex-col gap-3 sm:flex-row">
+          <a href="https://www.facebook.com/">
+            {" "}
+            <i class="ri-facebook-circle-line text-gray-400"></i>
+          </a>
+
+          <a href="mailto:nelson_salas@usmp.pe" target="_blank">
+            <i class="ri-mail-line text-gray-400 "></i>
+          </a>
+          <a href="https://www.instagram.com/#" target="_blank">
+            <i class="ri-instagram-line text-gray-400 "></i>
+          </a>
+          <a href="https://www.linkedin.com/in/#" target="_blank">
+            <i class="ri-linkedin-box-line text-gray-400 "></i>
+          </a>
+          <a href="https://github.com/nsalasz" target="_blank">
+            <i class="ri-github-line text-gray-400 "></i>
+          </a>
+        </div>
+        <div className="w-[1px] h-32 bg-[#125f63] sm:hidden"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSider;
