b76ff5bea5fa987129df745ccecd88b4cce542fe
nsalasz <leonelson951@gmail.com>
2024-11-01 16:21:21 -0500

    ad

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='p-5 bg-primary flex justify-between header'>
-        <h1 className='text-secondary text-4xl font-semibold'>N</h1>
+        <a href="/admin" ><h1 class="text-secondary text-4xl font-semibold">N</h1></a>
         <h1 className='text-white text-4xl font-semibold'>N</h1>
         <h1 className='text-tertiary text-4xl font-semibold'>S</h1>
         <h1 className='text-tertiary text-4xl font-semibold'>Z</h1>
